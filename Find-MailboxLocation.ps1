b460abc5fbd2dfea490eee903daf5f21b6e92f07
Victor Onofrei <39416712+victor-onofrei@users.noreply.github.com>
2021-02-05 21:04:41 +0200

    Remove test cmdlets

--- Find-MailboxLocation.ps1
+++ Find-MailboxLocation.ps1
@@ -1,8 +1,6 @@
 . "$PSScriptRoot\Initializer.ps1"
 $params = Invoke-Expression "Initialize-DefaultParams $args"
 
-# Write-Output $params.exchangeObjects
-
 Add-Content -Path $params.outputFilePath exchangeObject','mailboxLocation
 foreach ($exchangeObject in $params.exchangeObjects) {
     $exchangeObjectTypeDetails = (Get-Recipient -Identity $exchangeObject -ErrorAction SilentlyContinue).RecipientTypeDetails
@@ -15,5 +13,4 @@ foreach ($exchangeObject in $params.exchangeObjects) {
         $mailboxLocation = "N/A"
     }
     Add-Content -Path $params.outputFilePath $exchangeObject','$mailboxLocation
-    # Write-Output $exchangeObject','$mailboxLocation
 }
