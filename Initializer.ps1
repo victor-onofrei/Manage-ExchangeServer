victor-onofrei/Manage-ExchangeServer

commit d470e0e1d7f039cb0073560f01fb7e651a701ca3

Fix inexistent config path issue

Initializer.ps1
@@ -29,6 +29,8 @@ function Get-Config {
         # Read the existing config file.
         $config = Get-IniContent $configPath
     } else {
+        # Create .config folder if it doesn't exist
+        New-Item -Name ".config" -Path $HOME -Type Directory -ErrorAction SilentlyContinue
         # Create a default, empty config data.
         $config = [ordered]@{
             $configGenericCategory = @{}

commit 66ddefb197b7628825c4bb1375a670ed9c0dc42a

Fix incorrect param name
Introduced by d470e0e1.

Initializer.ps1
@@ -46,7 +46,7 @@ function Initialize-DefaultParams {
                 $config = Get-IniContent $configFilePath
             } else {
                 # Create the `.config` folder if it doesn't exist.
-                New-Item $configDirectoryPath -Type Directory -ErrorAction SilentlyContinue > $null
+                New-Item $configDirectoryPath -ItemType Directory -ErrorAction SilentlyContinue > $null
 
                 # Create a default, empty config data.
                 $config = [ordered]@{