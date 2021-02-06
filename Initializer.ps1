57a3e5f6e74ef8fa56d82d2fe1de5a78a7cb5ac2
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-02-06 13:41:59 +0200

    Refactor: Remove duplicate code

--- Initializer.ps1
+++ Initializer.ps1
@@ -128,7 +128,7 @@ function Initialize-DefaultParams {
         $inputFilePath = Join-Path $intermediateInputFilePath -ChildPath $InputFileName
         $outputFilePath = Join-Path $intermediateOutputFilePath -ChildPath $OutputFileName
         if (-not (Test-Path $outputFilePath -PathType Leaf)) {
-            New-Item -Name $OutputFileName -Path $intermediateOutputFilePath -ItemType File -ErrorAction SilentlyContinue > $null
+            New-Item $outputFilePath -ItemType File -ErrorAction SilentlyContinue > $null
         }
 
         $ExchangeObjects = Read-Param "ExchangeObjects" -Value $ExchangeObjects -DefaultValue (Get-Content $inputFilePath -ErrorAction SilentlyContinue) -Config $config -ScriptName $_ScriptName
