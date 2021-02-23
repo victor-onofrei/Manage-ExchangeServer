14c8a218bbe2ef013e2d621840591ebe65229d87
Victor Onofrei <39416712+victor-onofrei@users.noreply.github.com>
2021-02-23 15:12:53 +0200

    Add DisableEAP parameter to the Find-EAPEnabledMailbox script

--- Find-EAPEnabledMailbox.ps1
+++ Find-EAPEnabledMailbox.ps1
@@ -1,9 +1,44 @@
+param (
+    [Alias("DEAP")][Switch]$DisableEAP
+)
+
 begin {
     . "$PSScriptRoot\Initializer.ps1"
     $params = Invoke-Expression "Initialize-DefaultParams $args"
 }
 
 process {
+    if ($DisableEAP) {
+        $recipients = (
+            Get-Recipient -ResultSize 100) |
+            Where-Object {
+                $_.EmailAddressPolicyEnabled -eq $true -and
+                $_.CustomAttribute8 -like "world=H*" -or
+                $_.CustomAttribute8 -like "world=N*"
+            }
+        $recipientsCount = @($recipients).count
+        Write-Output "To process $recipientsCount recipients"
+        for ($index = 0; $index -lt $recipientsCount; $index++) {
+            Write-Output -InputObject `
+                "`tProcessing recipient " ($index + 1) " / $recipientsCount | $recipient"
+            $recipient = $recipients[$index]
+            $location = Get-ExchangeObjectLocation -ExchangeObject $recipient
+            switch ($location) {
+                ([ExchangeObjectLocation]::notAvailable) {
+                    Write-Output "Mailbox does not exist anymore. Skipping mailbox..."
+                }
+                ([ExchangeObjectLocation]::exchangeOnPremises) {
+                    # Set-Mailbox $recipient -EmailAddressPolicyEnabled $false -WhatIf
+                    Write-Output "$recipient is on prem"
+                }
+                ([ExchangeObjectLocation]::exchangeOnline) {
+                    # Set-RemoteMailbox $recipient -EmailAddressPolicyEnabled $false -WhatIf
+                    Write-Output "$recipient is in cloud"
+                }
+            }
+        }
+    }
+    $FormatEnumerationLimit = -1
     Get-Recipient -ResultSize Unlimited |
         Where-Object {
             $_.EmailAddressPolicyEnabled -eq $true -and
