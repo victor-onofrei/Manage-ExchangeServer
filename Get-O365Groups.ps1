5d3fde19fe3af93e2738e8f6ccc24a6fdcccaed1
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-03-04 22:58:50 +0200

    Refactor: Rename a variable

--- Get-O365Groups.ps1
+++ Get-O365Groups.ps1
@@ -78,6 +78,8 @@ process {
 
             $firstCompanyManagersOrMembersCount = $firstCompanyMembersCount
             $secondCompanyManagersOrMembersCount = $secondCompanyMembersCount
+
+            $groupsManagedBySMTP = ''
         }
 
         if (
