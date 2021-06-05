478d5c62bcc5592a6e0e97f827930044857c7478
Iulian Onofrei <iulian.onofrei@yahoo.com>
2021-06-05 22:36:16 +0300

    Refactor: Fix code style
    Co-authored-by: Victor Onofrei <39416712+victor-onofrei@users.noreply.github.com>

--- Get-MailTrafficReport.ps1
+++ Get-MailTrafficReport.ps1
@@ -1,21 +1,36 @@
-$serverName = $env:computername
-$beginDay = Get-Date -Hour 0 -Minute 00 -Second 00
-$endDay = $beginDay.AddDays(1).AddSeconds(-1)
-$outputPath = "C:\temp\" + $serverName + "_" + $beginDay.Day + "." + $beginDay.Month + "." + $beginDay.Year + ".csv"
+begin {
+    . "$PSScriptRoot\Initializer.ps1"
+    $params = Invoke-Expression "Initialize-DefaultParams $args"
+}
 
-Get-MessageTrackingLog -Start $beginDay -End $endDay -EventID "Receive" -ResultSize Unlimited |
-    Where-Object { (Get-ExchangeServer).Fqdn -notcontains $_.ClientHostname } |
-    Group-Object -Property ClientHostname, Sender |
-    ForEach-Object -Process {
-        New-Object -TypeName PSObject -Property @{
-            Timestamp = ($_.Group.Timestamp | Select-Object -First 1).ToString().Split(' ')[0]
-            Count = $_.Count
-            ClientHostname = $_.Name.Split(',')[0]
-            ClientComputedIP = (Resolve-DnsName -Name $_.Name.Split(',')[0] -Type A -ErrorAction SilentlyContinue |
-                Select-Object -ExpandProperty IPAddress -ErrorAction SilentlyContinue) -join ";"
-            Sender = $_.Name.Split(',')[1].Split(' ')[1]
-        }
-    } |
-    Sort-Object -Property Count -Descending |
-    Select-Object Timestamp,ClientHostname,ClientComputedIP,Sender,Count |
-    Export-Csv -Path $outputPath -NoTypeInformation
+process {
+    $serverName = $env:computername
+    $beginDay = Get-Date -Hour 0 -Minute 00 -Second 00
+    $endDay = $beginDay.AddDays(1).AddSeconds(-1)
+    $outputFileName = $params.$intermediateOutputFilePath +
+    $serverName + '_' + $beginDay.Day + '.' + $beginDay.Month + '.' + $beginDay.Year + '.csv'
+
+    Get-MessageTrackingLog -Start $beginDay -End $endDay -EventID 'Receive' -ResultSize Unlimited |
+        Where-Object { (Get-ExchangeServer).Fqdn -notcontains $_.ClientHostname } |
+        Group-Object -Property ClientHostname, Sender |
+        ForEach-Object -Process {
+            New-Object -TypeName PSObject -Property @{
+                Timestamp = ($_.Group.Timestamp | Select-Object -First 1).ToString().Split(' ')[0]
+                Count = $_.Count
+                ClientHostname = $_.Name.Split(',')[0]
+                Sender = $_.Name.Split(',')[1].Split(' ')[1]
+                ClientComputedIP =
+                (Resolve-DnsName `
+                        -Name $_.Name.Split(',')[0] `
+                        -Type A `
+                        -ErrorAction SilentlyContinue |
+                        Select-Object `
+                            -ExpandProperty IPAddress `
+                            -ErrorAction SilentlyContinue) `
+                        -join ';'
+                }
+            } |
+            Sort-Object -Property Count -Descending |
+            Select-Object Timestamp, ClientHostname, ClientComputedIP, Sender, Count |
+            Export-Csv -Path $outputFileName -NoTypeInformation
+}
