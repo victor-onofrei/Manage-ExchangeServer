3967a5d173076be2403d3369ee52c463bcdd7046
Victor Onofrei <39416712+victor-onofrei@users.noreply.github.com>
2021-07-02 11:39:38 +0300

    Add script to remove alias matching identifier

---
+++ Remove-SpecificAlias.ps1
@@ -0,0 +1,34 @@
+param (
+    [Alias('AI')][SupportsWildcards()][string]$AliasIdentifier
+)
+
+begin {
+    . "$PSScriptRoot\Initializer.ps1"
+    $params = Invoke-Expression "Initialize-DefaultParams $args"
+
+    $aliasIdentifierParams = @{
+        Name = 'AliasIdentifier'
+        Value = $AliasIdentifier
+        Config = $params.config
+        ScriptName = $params.scriptName
+    }
+    $aliasIdentifier = Read-Param @aliasIdentifierParams
+}
+
+process {
+    foreach ($exchangeObject in $params.exchangeObjects) {
+        $aliasToRemove = Get-Recipient $exchangeObject |
+            Select-Object -Property @{
+                Name = 'SpecificEmailAddresses';
+                Expression = {
+                    $_.EmailAddresses |
+                        Where-Object { $_ -like "$aliasIdentifier" }
+                    }
+                } | Select-Object -ExpandProperty SpecificEmailAddresses
+        foreach ($alias in $aliasToRemove) {
+            Set-RemoteMailbox $exchangeObject `
+                -EmailAddresses @{ remove = "$alias" } `
+                -EmailAddressPolicyEnabled $false
+        }
+    }
+}
