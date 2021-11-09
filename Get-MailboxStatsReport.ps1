0b91195c191781a3c99f0fd8af4da9d7fedbb25c
Victor Onofrei <39416712+victor-onofrei@users.noreply.github.com>
2021-11-09 13:57:36 +0200

    Add Get-MailboxStatsReport

---
+++ Get-MailboxStatsReport.ps1
@@ -0,0 +1,29 @@
+begin {
+    . "$PSScriptRoot\Initializer.ps1"
+    $params = Invoke-Expression "Initialize-DefaultParams $args"
+}
+
+process {
+    Get-Mailbox -ResultSize Unlimited |
+        Get-MailboxStatistics |
+        Select-Object -ExpandProperty TotalItemSize |
+        Select-Object -ExpandProperty Value |
+        Select-Object @{
+            Name = 'TotalItemSizeinMB';
+            Expression = {
+                [math]::Round(
+                    ($_.ToString().
+                        Split('(')[1].
+                        Split(' ')[0].
+                        Replace(',', '') / 1MB)
+                )
+            }
+        } |
+        Select-Object -ExpandProperty TotalItemSizeinMB |
+        Measure-Object -Average -Sum -Minimum -Maximum |
+        Export-Csv $params.outputFilePath -NoTypeInformation
+}
+
+end {
+    Send-DefaultReportMail -ScriptParams $params
+}
